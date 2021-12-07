e35eeef96592620fc2c59a5ec15f8a34e92afc6c
ThaFirstPaul <34832378+ThaFirstPaul@users.noreply.github.com>
2021-12-07 14:15:20 +0100

    Added day7

---
+++ day7/part2.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+
+var input_data = fs.readFileSync('./adventofcode/day7/input.txt').toString().split(",").map((n) => +n);
+
+// Returns the sum of all numbers 0 -> distance
+function fuel_distance(distance) {
+    var calculated = 0;
+    for (let i = 1; i < distance+1; i++) {
+        calculated += i;
+    }
+    return calculated;
+}
+
+// Find the largest number in the data
+var max = 0;
+for (let position_chosen = 0; position_chosen < input_data.length; position_chosen++) {
+    if (max < input_data[position_chosen]) {
+        max = input_data[position_chosen]
+    }
+}
+
+// create an array max+1 wide, each position is the # of crabs at that point
+var horizontal_array = Array(max + 1).fill(0)
+
+// calculate the max fuel_used possible
+var cheapest_option = max*fuel_distance(max);
+
+// add all the crabs from input_data to the array
+for (let i = 0; i < input_data.length; i++) {
+    const crab_loc = input_data[i];
+
+    horizontal_array[crab_loc]++;
+}
+
+// loop through each postion to check if it is the cheapest option
+for (let test_pos = 0; test_pos < horizontal_array.length; test_pos++) {
+    var fuel_used = 0;
+
+    for (let crab_i = 0; crab_i < horizontal_array.length; crab_i++) {
+        const no_of_crabs = horizontal_array[crab_i];
+        const distance = fuel_distance(Math.abs(crab_i - test_pos));
+
+        fuel_used += distance * no_of_crabs;
+    } 
+    if (cheapest_option > fuel_used) {
+        cheapest_option = fuel_used
+    }
+}
+
+console.log(`Fuel used = ${cheapest_option}`)
